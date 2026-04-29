dd71f656df10114e9d7ce0cbdb91e093c751f35a
Harishh1947 <163561344+Harishh1947@users.noreply.github.com>
2026-04-29 22:50:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -300,5 +300,5 @@ def admin():
 
 
 # 🚀 Run
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
